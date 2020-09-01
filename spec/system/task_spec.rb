38b82960e37e2f65b0b88957bf5dc5af223609a2
yshici <lo.ol.000.yt@gmail.com>
2020-09-01 13:04:57 +0900

    [Fix] Factoryにassociationを定義

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   let(:project){ create(:project) }
-  let(:task){ create(:task, project_id: project.id) }
-  let(:task_done){ create(:task, :done, project_id: project.id) }
+  let(:task){ create(:task) }
+  let(:task_done){ create(:task, :done) }
 
   describe 'Task一覧' do
     context '正常系' do
